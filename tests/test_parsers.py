2ced65c71890cda0786bf7e2d9341613b33ff2d5
noam teyssier <noam.teyssier@pm.me>
2021-12-17 15:30:22 -0800

    added some example test functions

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -1,1 +1,19 @@
 # write tests for parsers
+
+from seqparser import (
+        FastaParser,
+        FastqParser)
+
+
+def test_freebie_parser_1():
+    """
+    This one is a freebie
+    """
+    assert True
+
+
+def test_freebie_parser_2():
+    """
+    This too is a freebie
+    """
+    assert 1 != 2
--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -1,1 +1,19 @@
 # write tests for transcribes
+
+from seqparser import (
+        transcribe,
+        reverse_transcribe)
+
+
+def test_freebie_transcribe_1():
+    """
+    This one is a freebie
+    """
+    assert True
+
+
+def test_freebie_transcribe_2():
+    """
+    This too is a freebie
+    """
+    assert 1 != 2
