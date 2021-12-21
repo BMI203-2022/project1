ad6aafce60eb1fab236ceb17ec3cdb880b551818
Parker Grosjean <44449176+pgrosjean@users.noreply.github.com>
2021-12-21 12:47:13 -0800

    Update test_transcribe.py

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -8,6 +8,7 @@ from seqparser import (
 def test_freebie_transcribe_1():
     """
     This one is a freebie
+    DO NOT MODIFY THIS FUNCTION
     """
     assert True
 
@@ -15,5 +16,22 @@ def test_freebie_transcribe_1():
 def test_freebie_transcribe_2():
     """
     This too is a freebie
+    DO NOT MODIFY THIS FUNCTION
     """
     assert 1 != 2
+
+        
+def test_transcribe():
+    """
+    Write your unit test for the
+    transcribe function here.
+    """
+    pass
+
+
+def test_reverse_transcribe():
+    """
+    Write your unit test for the
+    reverse transcribe function here.
+    """
+    pass
